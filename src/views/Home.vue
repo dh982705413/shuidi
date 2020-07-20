33a37941ebe77025a9636c68281790fbebd2ae34
dh <982705413@qq.com>
2020-07-20 09:49:03 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -13,6 +13,12 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    dh() {
+      const dh2 = 12
+      console.log(dh2)
+    }
   }
 }
 </script>
